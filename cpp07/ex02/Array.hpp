ba91949c2b4dc35116407f4ad1dfd6f5b3ac40de
Nouhayla Lahmar <nolahmar@e1r8p11.1337.ma>
2024-02-29 16:22:10 +0100

    fjff

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: nolahmar <nolahmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 11:29:25 by nolahmar          #+#    #+#             */
-/*   Updated: 2024/02/14 15:45:35 by nolahmar         ###   ########.fr       */
+/*   Updated: 2024/02/29 15:51:59 by nolahmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,46 +17,47 @@
 
 template <typename T>
 class Array {
-  private:
-    T* array;
-    unsigned int n;
-  public:
-    Array():n(0) {
-      array = new T[0];
-    }
+	private:
+		T* array;
+		unsigned int n;
+	public:
+		Array():n(0) {
+			array = new T[0];
+		}
+		
+		Array(unsigned int n):n(n) {
+			array = new T[n];
+		}
     
-    Array(unsigned int n):n(n) {
-      array = new T[n];
-    }
-    
-    Array(Array const & src) : n(src.n) {
-      array = new T[n];
-        for (unsigned int i = 0; i < n; i++)
-          array[i] = src.array[i];
-        }
-    ~Array() {
-      delete[] array;
-    }
+		Array(Array const & src) : n(src.n) {
+			//delete[] array;
+			array = new T[n];
+			for (unsigned int i = 0; i < n; i++)
+				array[i] = src.array[i];
+		}
+		~Array() {
+			delete[] array;
+		}
         
-    Array & operator=(Array const & other) {
-    if (this != &other)
-      {
-        n = other.n;
-        array = new T[n];
-        for (unsigned int i = 0; i < n; i++)
-           array[i] = other.array[i];
-      }
-    }
+		Array & operator=(Array const & other) {
+			if (this != &other)
+			{
+				n = other.n;
+				array = new T[n];
+				for (unsigned int i = 0; i < n; i++)
+					array[i] = other.array[i];
+			}
+		}
     
-    T & operator[](unsigned int i) const {
-      if (i >= n)
-        throw std::exception();
-          return array[i];
-    }
-    
-    unsigned int size() const {
-      return n;
-    }
+		T & operator[](unsigned int i) const {
+			if (i > n) // if(i >= n)
+				throw std::exception();
+			return array[i];
+		}
+		
+		unsigned int size() const {
+			return n;
+		}
 };
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nolahmar <nolahmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 11:29:03 by nolahmar          #+#    #+#             */
-/*   Updated: 2024/02/14 15:34:17 by nolahmar         ###   ########.fr       */
+/*   Updated: 2024/02/29 15:50:05 by nolahmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int main()
 {
-    Array<int> a(10);
+    Array<int>a(6);
     
     try
     {
-        a[5] = 8;
+        a[6] = 8;
         a[4] = 5;
-        std::cout << a[5] << std::endl;
+        std::cout << a[6] << std::endl;
         std::cout << a[4] << std::endl;
         std::cout << a[50] << std::endl;
     }
@@ -28,5 +28,21 @@ int main()
     {
         std::cout << "index is out of range" << std::endl;
     }
+
+     Array<char> b(10);
+    
+    try
+    {
+        b[5] = 'a';
+        b[4] = 'b';
+        std::cout << b[5] << std::endl;
+        std::cout << b[4] << std::endl;
+        std::cout << b[50] << std::endl;
+    }
+    catch(std::exception &e)
+    {
+        std::cout << "index is out of range" << std::endl;
+    }
+    system("leaks Array");
     return (0);
 }
